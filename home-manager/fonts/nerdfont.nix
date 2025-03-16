3b015168eb5f494e96e84859cfba6070bc3ec016
Tudor Croitoru <ihorvalds@outlook.com>
2025-03-16 07:55:50 +0000

    Remove jetbrains editors, try adding emoji fonts

--- home-manager/fonts/nerdfont.nix
+++ home-manager/fonts/nerdfont.nix
@@ -1,6 +1,19 @@
 { config, pkgs, ...}:
 {
-  fonts.fontconfig.enable = true;
+  fonts.fontconfig = {
+    enable = true;
+    defaultFonts = {
+      sansSerif = [
+        "Noto Sans"
+        "Noto Color Emoji"
+      ];
+      monospace = [
+        "JetBrainsMono Nerd Font"
+        "Noto Color Emoji"
+      ];
+      emoji = [ "Noto Color Emoji" ];
+    };
+  };
 
   home.packages = [
     pkgs.nerd-fonts.fira-code
--- home-manager/tools/extra-packages.nix
+++ home-manager/tools/extra-packages.nix
@@ -15,9 +15,5 @@
     ghostty
     fzf-make
     fzf-git-sh
-
-    # JetBrains editors. They're good
-    jetbrains.rust-rover
-    jetbrains.clion
   ];
 }
