e33db94e3cf204b43b10a050f9fc249e4cc6c1ee
IHorvalds <ihorvalds@outlook.com>
2025-05-11 20:26:26 +0100

    Add scrollbar, smooth scroll and tabs

--- nvim/lua/plugins/lualine.lua
+++ nvim/lua/plugins/lualine.lua
@@ -1,8 +1,8 @@
 return {
-    "nvim-lualine/lualine.nvim",
-    dependencies = {
-        "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim",
-        "olivercederborg/poimandres.nvim"
-    },
-    opts = {theme = "poimandres"}
+  "nvim-lualine/lualine.nvim",
+  dependencies = {
+    "nvim-tree/nvim-web-devicons", "f-person/git-blame.nvim",
+    "olivercederborg/poimandres.nvim"
+  },
+  opts = { theme = "poimandres" }
 }
